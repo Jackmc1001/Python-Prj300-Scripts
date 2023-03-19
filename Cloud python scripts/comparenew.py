596e991dbc431db3572bbd4b67e171e580721c15
Jackmc1001 <Jackmcelroy2010@hotmail.com>
2023-03-19 20:35:35 +0000

    Final Push to main

--- Cloud python scripts/comparenew.py
+++ Cloud python scripts/comparenew.py
@@ -1,4 +1,9 @@
+#enviornment variables - room no
+#system variables - mac and ip address
+
+import socket
 import boto3
+import time
 import io
 from PIL import Image
 import cv2
@@ -21,7 +26,7 @@ def capture_and_save_image(cam_port=0):
     if result:
         cv2.imshow(image_name, image)
         cv2.imwrite(image_name+".jpg", image)
-        cv2.waitKey(0)
+        #cv2.waitKey(0)
         cv2.destroyWindow(image_name)
         cam.release()
         return image_name
@@ -42,8 +47,7 @@ def search_faces(image_name):
     image_binary = stream.getvalue()
 
     # searches the collection for the face
-    FaceDetected = True
-    
+    FaceDetected = True 
     try:
         response = rekognition.search_faces_by_image(
                 CollectionId='Prj300Rekognition',
@@ -52,8 +56,6 @@ def search_faces(image_name):
     except:
         print("no faces detected" )
         FaceDetected = False 
-        
-
     found = False
     
     #create a list to send image and metadata
@@ -62,8 +64,8 @@ def search_faces(image_name):
     #for loop if multple images are seen
     if FaceDetected == True:
         for match in response['FaceMatches']:
-            matchconfidence = round(match['Face']['Confidence'], 5 )
-            print(matchconfidence)
+            matchconfidence = round(match['Face']['Confidence'], 2 )
+            #print(matchconfidence)
 
             #uses the RekognitionId to find the face in dynamo
             face = dynamodb.get_item(
@@ -77,17 +79,20 @@ def search_faces(image_name):
             if 'Item' in face:
                 personName =face['Item']['FullName']['S']
                 StudentNumber = face['Item']['StudentNo']['S']
-                print ("Found Person: Name:", face['Item']['FullName']['S'], ", Student No:", face['Item']['StudentNo']['S'])
                 
+                print("Access Granted!")
                 found = True
                 sendtos3(image_name+'.jpg', entry_date, entry_time, personName, StudentNumber,matchconfidence ,1)
-            #return found
             
             
-        if not found:
+            
+        if found == False:
             print("Person cannot be recognized")
-            sendtos3(image_name+'.jpg', entry_date, entry_time, "Unkown", "Unkown", "Null" ,0)
-            
+            print("Access Denied")
+            sendtos3(image_name+'.jpg', entry_date, entry_time, "Unkown", "Unkown", "0" ,0)
+    
+    
+        os.remove(image_name+".jpg")  
             
 def sendtos3(Imagename, Date, Time, Name, studentNo, result, Pass):
     file = open(Imagename,'rb')
@@ -95,12 +100,27 @@ def sendtos3(Imagename, Date, Time, Name, studentNo, result, Pass):
     ret = object.put(Body=file, Metadata={'Date': Date,'Time': Time,'FullName':Name, 'StudentNo':studentNo, 'Match':str(result), 'Pass':str(Pass)})
         
 def main():
+    x = True;
+    hostname=socket.gethostname()   
+    IPAddr=socket.gethostbyname(hostname)   
+    #print("Your Computer Name is:"+hostname)   
+    print("Your Computer IP Address is:"+IPAddr) 
+    while x == True:
+        key_entry = "value"
+        #time.sleep(3)
+        key_entry = input("Hit the Enter button to continue")
+        if key_entry == "":
+        
+            Image_Name = capture_and_save_image()
+            search_faces(Image_Name)
+            
+            #time.sleep(3)
+        else:
+            time.sleep(3)
+            
     
-    Image_Name = capture_and_save_image()
     
-    search_faces(Image_Name)
-    
-    os.remove(Image_Name+".jpg")
 
 if __name__ == "__main__":
     main()
+
